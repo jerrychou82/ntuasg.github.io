1e0f15c4f7a2535cf48fb6704fdc137583fd91c6
jerrychou82 <jerrychou82@gmail.com>
2017-01-29 22:03:27 +0800

    nav color

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var mySidenav = document.getElementById("mySidenav");
 var overlayBg = document.getElementById("myOverlay");
 
 // Toggle between showing and hiding the sidenav, and add overlay effect
-function w3_open(pageName) {
+function w3_open(evt,pageName) {
     var i;
     var x = document.getElementsByClassName("w3-main");
     
@@ -13,8 +13,16 @@ function w3_open(pageName) {
         x[i].style.display = "none";
         x[i].style.opacity = 0.5; 
     }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" transbox", "");
+        tablinks[i].className += " transbox";
+    }
+
     document.getElementById(pageName).style.display = "block"; 
-    document.getElementById(pageName).style.opacity = 1; 
+    document.getElementById(pageName).style.opacity = 1;
+    evt.currentTarget.className = evt.currentTarget.className.replace(" transbox", "");
 }
 
 // Close the sidenav with the close button
